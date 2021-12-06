aea3bdc2b9a8bdc7a6a3b0d34389460f68a2b115
Cesar Menjivar <cemenjiv@student.42quebec.com>
2021-12-06 12:02:13 -0500

    Correction ft_print.h suivant merge des branchs

--- ft_printf.h
+++ ft_printf.h
@@ -1,8 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cemenjiv <cemenjiv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/06 12:01:29 by cemenjiv          #+#    #+#             */
+/*   Updated: 2021/12/06 12:01:33 by cemenjiv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
+# include <stdio.h>
+# include <stdarg.h>
+# include "libft/libft.h" 
 
-size_t	ft_strlen(const char *s);
-char	*ft_itoa(int n);
-void	ft_putchar_fd(char c, int fd);
-void	ft_putnbr_fd(int n, int fd);
-void	ft_putstr_fd(char *s, int fd);
+int		ft_printf(const char *str, ...);
+void	ft_putchar_new(char c, int *count);
+void	ft_putnbr_new(int n, int *count);
+void	ft_putnbr_new1(int n, int *count);
+void	ft_putstr_new(char *s, int *count);
+void	ft_putnbr_addr_hex(unsigned long nbr, int *count);
+void	ft_putnbr_hex_low(long long nbr, int *count);
+void	ft_putnbr_hex_upp(long long nbr, int *count);
+
+#endif
